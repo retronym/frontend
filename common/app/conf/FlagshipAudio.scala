fddf7e9d52b77ca131c21901b420c946c6d60749
Anna Leach <anna,leach@theguardian.com>
2018-10-08 17:50:19 +0100

    container and header

---
+++ common/app/conf/FlagshipAudio.scala
@@ -0,0 +1,16 @@
+package conf
+
+object FlagshipAudio {
+  val tagName = "culture/series/thestart"
+  val title = "The Start"
+  val descriptionLong = "The Start is a podcast about artistic beginnings, as told by great artists of our time. Focussing on one piece; they share how these early moments of creativity shaped them"
+  val descriptionShort = "The Start is a podcast about artistic beginnings."
+  val seriesId = "the-start"
+  val imageUrl = "journalism/audio-tag-page/the-start.png"
+  val backgroundColor = "#fbcdd6"
+  val accentColor = "#3a3a3a"
+  val subscribeLinks = Map(
+    "Apple Podcasts" -> "https://itunes.apple.com/us/podcast/id1337773497",
+    "Google Podcasts" -> "https://www.google.com/podcasts?feed=aHR0cHM6Ly93d3cudGhlZ3VhcmRpYW4uY29tL2N1bHR1cmUvc2VyaWVzL3RoZXN0YXJ0L3BvZGNhc3QueG1s",
+    "Spotify" -> "https://open.spotify.com/show/1Y95YPvBTyQTLxTNQalQcZ")
+}
